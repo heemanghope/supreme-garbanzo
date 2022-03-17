20b6f85651ffaad870a06498484e872dfa159961
박희망 <hope30529@gmail.com>
2022-03-17 18:18:06 +0900

    멤버 메서드 제한 / 예시

---
+++ pro05/src/exam01/Person.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Person {
+     public String pNO;
+     private String name;
+     private char gender;
+     private String adderss;
+     private String phone;
+     private int age;
+     
+     
+}
---
+++ pro05/src/exam03/pack2/AccessChild.java
@@ -0,0 +1,15 @@
+package exam03.pack2;
+
+import exam03.pack1.AccessParent;
+
+public class AccessChild extends AccessParent {
+
+	
+	
+	  public void method( ) {
+		  System.out.println(p1);
+		  System.out.println(p2);
+		  System.out.println(p3);
+		  System.out.println(p4);
+	  }
+}
