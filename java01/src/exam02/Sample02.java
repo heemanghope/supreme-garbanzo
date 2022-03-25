58081d5f821e042b5bb77f5e68e3d38921e72a20
박희망 <hope30529@gmail.com>
2022-03-25 23:46:17 +0900

    반복문 예제 복습

---
+++ java01/src/exam02/Sample02.java
@@ -0,0 +1,34 @@
+package exam02;
+
+import java.util.*;
+
+public class Sample02 {static void main(String[] args) {
+	int num;
+	int sum =0;
+	boolean flag =true;   //while문의 조건식으로 사용될 변수.
+	
+	Scanner scanner =new Scanner(System.in);
+	
+	System.out.println("합계를 구할 숫자를 입력하세요.(끝내려면 0을 입력)");
+	
+	while(flag) {      //flag의 값이 true이므로 조건식은 참이된다.
+		System.out.print(">>");
+		
+		
+		String tmp = scanner.nextLine();
+		num = Integer.parseInt(tmp);
+		
+		if(num!=0) {
+			sum+= num; //num이 0이 아니면, sum을 더한다.
+		}else {
+			flag =false;  //num이 0이면 ,flag의 값을 flase로 변경한다.
+		}
+		
+		
+	}//while문의 끝
+	
+	
+          System.out.println("합계:"+sum);
+           }
+
+        }
