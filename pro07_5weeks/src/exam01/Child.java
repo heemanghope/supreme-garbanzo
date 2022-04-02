d26b1efb7fa6e429f9786357b877680a276b3635
박희망 <hope30529@gmail.com>
2022-04-02 18:56:45 +0900

    상속 부모.자식

---
+++ pro07_5weeks/src/exam01/Child.java
@@ -0,0 +1,25 @@
+package exam01;
+
+public class Child extends Parent {//부모의 상속만 취한다. //자식이 부모의 대한 생성도 한다.-
+//부모가 시키는 대로 쓴다.// 상속관계에서는 기본생성자가 필요! 반드시 필요한것은 아님.
+
+	public Child(int number) {
+		super(10);//부모생성자 명시 ******조심*******
+		
+//노션 메모보기.
+	}
+	
+	//안해도 상관 없지만 오버라이딩이 가능한 메서드인지 확인하는 용도
+	@Override//리턴타입, 매개변수,메서드 동일한지????? 전부다 제대로 보기!!!!!!!
+  public int getNumber() {//super을 안쓰면 자식이 우선임 //부모를 우선시킬꺼면 super 명시하기
+	  
+		System.out.println("자식의 getNumber 메서드 시작");
+		int num = super.getNumber();
+		System.out.println("자식의 getNumber 메서드 끝");
+        return num +10; //오버라이드 : 부모가 가지고 있는 메서드와 동일하게 정의한다.
+	  //int 반환타입 ()매개변수 , 접근제한자도 모두 동일하게 사용
+	  // 기존 부모의 기능을 재정의 할 수 있음.//super는 부모를 명시하는것이다. 나한테의 getnumber가 아니라, 부모가가지고 있은것 재정의하는것이다.
+  }  //super 새로운 것으로 정의할것이다. 나만의 기능은 super 노노..
+  //부모가 가지고 있는거에서 동작시킨다! 이러면 부모의 메서드로 만드는거면 super를 붙이기.
+
+}
---
+++ pro07_5weeks/src/exam01/Parent.java
@@ -0,0 +1,23 @@
+package exam01;
+
+public class Parent { //캡슐화 private 접근제한자 //같은 클래스 안에서만 가능하다.접근불가능 
+	//상속은 되어있는데 접근제한자가 private -ex. 부모님 지갑에서 허락. 1000원 ->가져가 ,허락이 public 
+	//간접접근...getter setter
+   private int number =10; //직접적으로는 안되지만 요청하면 해주겟다.
+   
+   
+   public Parent(int number) {
+	   this.number =number; //생성자는 상속이 안됩니다. //부모가 매개변수가 있는 생성자를 생성하게 되면 기본 생성자는 자동으로 생성되지 않는다.
+	   
+	   
+   }
+   public int getNumber() {//==
+	  System.out.println("부모의 getNumber 메서드 시작");
+	  System.out.println("부모의 getNumber 메서드 끝");
+	  return number;
+   }
+   
+   public void setNumber(int number) {//허락을 구하는것.   private,public 모두 상속가능 단, 직접 private접근이 안됨.
+	   this.number=number;
+   }
+}
