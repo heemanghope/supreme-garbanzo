688bd4875dc19dcf593e94dd54ef350862cfb8c7
박희망 <hope30529@gmail.com>
2022-04-05 15:37:19 +0900

    객체 배열 활용문제

--- pro06/src/exam04/Grade.java
+++ pro06/src/exam04/Grade.java
@@ -1,23 +1,15 @@
 package exam04;
 
 public class Grade {
+	
 	private String name;
-	private double score;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	private  double score;
+	
+	
+	
 	public Grade(String name) {
 		super();
 		this.name = name;
-	}
-	public double getScore() {
-		return score;
-	}
-	public void setScore(double score) {
-		this.score = score;
 	}
 
 	public Grade(String name, double score) {
@@ -25,5 +17,24 @@ public class Grade {
 		this.name = name;
 		this.score = score;
 	}
+
+	public String getName() {
+		return name;
+	}
 	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getScore() {
+		return score;
+	}
+	
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+	
+	
+
 }
