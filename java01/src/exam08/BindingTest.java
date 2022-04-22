b1be1ed8c4e48a8b6454af8073c393e04abb2bd8
박희망 <hope30529@gmail.com>
2022-04-22 14:36:10 +0900

    상속/예제 연습하기

---
+++ java01/src/exam08/BindingTest.java
@@ -0,0 +1,19 @@
+package exam08;
+
+public class BindingTest {
+
+	public static void main(String[] args) {
+		
+		class product{
+			int price;
+			int bonusPoint;
+			
+		}
+        class Tv extends Product {}
+        class Computer extends Product{}
+        class Audio extends Product {}
+        
+      
+	}
+
+}
