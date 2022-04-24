ec939e9f2f151d9c16c48c022b9334eecd8a54c5
박희망 <hope30529@gmail.com>
2022-04-24 22:13:21 +0900

    여러 종류의 객체를 배열로 다루기-예제

---
+++ java01/src/exam09/Main.java
@@ -0,0 +1,22 @@
+package exam09;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Buyer b = new Buyer();
+		Tv tv = new Tv();
+		Computer com = new Computer();
+		Audio audio = new Audio();
+		
+		b.buy(tv);
+		b.buy(com);
+		b.buy(audio);
+		b.summary();
+		System.out.println();
+		b.refund(com);
+		b.summary();
+		//
+
+	}
+
+}
