5c06558dce1f9f795c760581a7dcd1c946074344
박희망 <hope30529@gmail.com>
2022-05-30 20:15:13 +0900

    dbconnection

--- db_test/src/db_com/com/db/DBConn.java
+++ db_test/src/db_com/com/db/DBConn.java
@@ -3,40 +3,55 @@ package db_com.com.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DBConn {
-  
-	
-	public void localConnect() throws Exception{
-	 
+public class DBConn{
+		  
+		private final static String DRIVER_NAME = "oracle.jdbc.driver.OracleDriver";
+		private final static String BASE_URL = "jdbc:oracle:thin:@";
+		
+		
+		private String url_address;
+		private Connection conn;
+		private Statement stat;
+		
+		
+		public DBConn(String address, String port, String serviceName, String username, String password) throws Exception {
+			url_address = String.format("%s:%s/s", address,port, serviceName);
+			this.createConnection(username, password);
+		}
+		private void createConnection( String username, String password) throws Exception {
+	   public void localConnect() throws Exception {
+		
 		// 1.Driver 등록
-	Class.forName("oracle.jdbc.driver.OracleDriver");
+		Class.forName(DRIVER_NAME);
+		
+		// 2.DBMS 연결
+		conn = DriverManager.getConnection( BASE_URL + url_address,username ,password);	
+		
+		// 3.Statement 생성
+	    stat = conn.createStatement();
 	 
-	// 2.DBMS 연결
-	Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XE", "puser1","puser1");	
-	  
-	// 3.Statement 생성
-	 Statement stat = conn.createStatement();
 	 
-    // 4. SQL 질의문 전송 및 반환
-	ResultSet rs = stat.executeQuery("SELECT 'Hello' FROM DUAL");
-	if(rs.next()) {
-		System.out.println(rs.getString(1));
-	}
-	 // 5. 객체 반환	
-    rs.close();
-    stat.close();
-    conn.close();
-    
+	 
+	 
+	 public ResultSet sendSelectQuery(String query) throws Exception{ //send는 ResultSet으로 반환한다.
+			return this.stat.executeQuery(query);
+		}
+		public int sendInsertQuery(String query)  throws Exception{ // INSERT,UPDATE,DELETE는 INT형으로 반환한다.
+			return this.stat.executeUpdate(query);
+		}
+		public int sendUpdateQuery(String query)  throws Exception{
+			return this.stat.executeUpdate(query);
+		}
+		public int sendDeleteQuery(String query)  throws Exception{
+			return this.stat.executeUpdate(query);
+		}
 		
-		
-	}
-		public static void main(String[] args) throws Exception {
-			DBConn myDB = new DBConn();
-			myDB.localConnect();
-		
-	}
+		 public void close()  throws Exception{
+		    	this.stat.close();
+		    	this.conn.close();
+		}
+	
 }
 
