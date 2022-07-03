e044fb677967a692e53affafba7dcfc5fd1e5b4c
박희망 <hope30529@gmail.com>
2022-07-03 21:06:42 +0900

    event 동작

---
+++ web_pro09/src/main/webapp/static/js/sample06.js
@@ -0,0 +1,13 @@
+/**
+*
+ */
+ 
+ function f1() {
+	 var id1= document.getElementById("id1");
+	 id1.innerText= "클릭  이벤트 동작!!";
+}
+ 
+ function f2(element) {
+	element.innerText ="클릭 이벤트 동작!!";
+}
+//매개변수 x ,y element 다 상관없음
